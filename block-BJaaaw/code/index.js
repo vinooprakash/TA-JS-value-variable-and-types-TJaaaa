c46d26373e22c06afd54a4d2400a5ec2645bc965
vinooprakash <vinooprakash93@gmail.com>
2021-03-07 23:57:27 +0530

    Decision and Loops - Block 2

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -7,9 +7,31 @@
   - `if` the number is odd print the message "number is odd"
 */
 
+let num = Number(prompt('Enter a number'));
+if (num % 2 === 0) {
+  alert('Number is even');
+}
+else {
+  alert('Number is odd');
+}
+
 // 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
 
+let num1 = Number(prompt('Enter value of number 1'));
+let num2 = Number(prompt('Enter value of number 2'));
+if (num1 > num2) {
+  alert(`${num1} is greater than ${num2}`);
+}
+else {
+  alert(`${num2} is greater than ${num1}`);
+}
+
 // 3. Convert the above code using`?` terniary operator
+
+num1 > num2 ? 
+alert(`${num1} is greater than ${num2}`) 
+: 
+alert(`${num2} is greater than ${num1}`)
 
 /*
 4. Write a program that asks the user for the house name and check the following conditions:
@@ -19,11 +41,54 @@
   - `else` print the message " All men must die"
 */
 
+let houseName = prompt('Please enter your House Name');
+if (houseName === "stark") {
+  alert('Winter is coming');
+}
+else if(houseName === "lannister") {
+  alert('A Lannister always pays his debts');
+}
+else {
+  alert('All men must die');
+}
+
 // 5. Convert the above code using`?` terniary operator
+
+houseName === "stark" ?
+alert('Winter is coming')
+:
+houseName === "lannister" ?
+alert('A Lannister always pays his debts')
+:
+alert('All men must die')
 
 // Switch
 
+switch(houseName) {
+  case ("stark"): 
+    alert('Winter is coming');
+    break;
+  case ("lannister"):
+    alert('A Lannister always pays his debts');
+    break;
+  default:
+    alert('ALl men must die');
+}
+
 // 6. Write a program that takes the number of the month(1 - 12) and alert number of days in the month.
+
+let monthNum = Number(prompt("Enter the month name"));
+if ((monthNum >= 1) && (monthNum <= 12) && (monthNum != 2)) {
+  if (monthNum % 2 == 0)  {
+    alert('The number of days in this month is 30');
+  }
+  else {
+    alert('The number of days in this month is 31');
+  }
+}
+else if (monthNum == 2) {
+  alert('The number of days in this month is 28');
+}
 
 /* 7.
 - Write a program that take the salery of the user using prompt and alert the in -hand amount.You will find out the in -hand amount by deducting the tax amoun from salery.Conditions are given below.
@@ -32,6 +97,12 @@
   - `Salary > 50000` tax is 30 %
 
 */
+
+let totalSalary = Number(prompt('Enter the total salary'));
+if (totalSalary <=20000) {
+  let inHand = totalSalary - (10 * totalSalary)/100;
+  alert(`'In Hand salary is' ${inHand}`);
+}
 
 //  if..else vs switch
 
@@ -46,6 +117,46 @@ Implement the condition give below using`if..else` and`switch` statement.
 
 */
 
+// If-Else
+
+let marks = Number(prompt('Enter the marks obtained'));
+if (marks > 100) {
+  alert(`"Marks can't be greater than 100"`);
+}
+else if ((marks > 80) && (marks < 100)) {
+  alert(`Grade A`);
+}
+else if ((marks > 50) && (marks < 80)) {
+  alert(`Grade B`);
+}
+else if ((marks > 30) && (marks < 50)) {
+  alert(`Grade C`);
+}
+else if (marks >= 0) {
+  alert(`Grade D`);
+}
+
+// Switch
+
+switch(marks) {
+  case (marks > 100): 
+    alert(`"Marks can't be greater than 100"`);
+    break;
+  case ((marks > 80) && (marks < 100)):
+    alert(`Grade A`);
+    break;
+  case ((marks > 50) && (marks < 80)):
+    alert(`Grade B`);
+    break;
+  case ((marks > 30) && (marks < 50)):
+    alert(`Grade C`);
+    break;
+  case (marks >= 0):
+    alert(`Grade D`);
+  default:
+    alert('Invalid number');
+}
+
 /* 9. Weather app
 
   - Ask user to provide the condition of the weather outside by asking `What is the weather like outside?`
@@ -55,3 +166,17 @@ Implement the condition give below using`if..else` and`switch` statement.
   - If`freezing` alert`Get your sweeter on`
   - Anything else should alert`Not a valid input`
 */
+
+let weather = prompt(`What is the weather like outside?`);
+if (weather == "sunny") {
+  alert('Wear a T-shirt');
+}
+else if(weather == "rainy") {
+  alert(`Dont't forget to take a raincoat`);
+}
+else if (weather == "freezing") {
+  alert('Get your sweater on');
+}
+else {
+  alert('Not a valid input');
+}
