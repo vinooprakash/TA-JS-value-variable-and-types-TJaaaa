737f0f37abef554b0747e0dda40843c19d98f2e1
vinooprakash <vinooprakash93@gmail.com>
2021-03-10 12:01:07 +0530

    Decision & Loops - Block 4

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,40 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for(let i = 1; i<=10; i = i + 1) {
+  console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
+
+for(i = 1; i<=10; i = i + 1) {
+  if(i % 2 == 0){
+    console.log(i);
+  }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
 
+for(i = 1; i<=10; i = i + 1) {
+  if(i % 2 !== 0){
+    console.log(i);
+  }
+}
+
 // Calculate the sum of all numbers from 1 to 10.
 
+let sum =0;
+for(i = 1; i<=10; i = i + 1) {
+  sum = sum + i;
+}
+console.log(sum);
+
 // Log all the values from 1 to 10 using while loop
+
+i = 1;
+while (i <= 10) {
+  console.log(i);
+  i = i + 1; 
+}
+
+
